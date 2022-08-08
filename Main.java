5a403876a64b60adade43d453107bb95d50d014a
Joalison Matheus <joalison.ferreira@academico.ifpb.edu.br>
2022-08-07 23:00:47 -0300

    added registerPlayer on Main

--- Main.java
+++ Main.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+import java.text.ParseException;
+
 public class Main {
-  public static void main(String[] args) {
+  private static Jogador cadastrarJogador(BatalhaNaval tabuleiro) throws ParseException {
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.print("Nome do jogador: ");
+    String nome = teclado.nextLine();
+
+    Jogador jogador = new Jogador(nome, tabuleiro);
+
+    return jogador;
+  }
+
+  public static void main(String[] args) throws ParseException {
     BatalhaNaval tabuleiroJogador1 = new BatalhaNaval();
     BatalhaNaval tabuleiroJogador2 = new BatalhaNaval();
 
-    Jogador euridyce = new Jogador("Euridyce", tabuleiroJogador1);
-    Jogador joalison = new Jogador("Joalison", tabuleiroJogador2);
+    System.out.println("Cadastro do primeiro jogador");
+    Jogador jogador1 = cadastrarJogador(tabuleiroJogador1);
+    System.out.println("\nCadastro do segundo jogador");
+    Jogador jogador2 = cadastrarJogador(tabuleiroJogador2);
 
-    RodadaDeTiros rodadaDeTiros = new RodadaDeTiros(euridyce, joalison);
+    RodadaDeTiros rodadaDeTiros = new RodadaDeTiros(jogador1, jogador2);
 
     System.out.println("Jogador 1 - Adicionar armas");
-    euridyce.adicionarArmas();
+    jogador1.adicionarArmas();
 
     System.out.println("\n\nJogador 2 - Adicionar armas");
-    joalison.adicionarArmas();
+    jogador2.adicionarArmas();
 
     rodadaDeTiros.trocaDeTiros();
   }
--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 - Cada jogador possui um tabuleiro
 - No primeiro tabuleiro, cada jogador distribui, em seu próprio papel, as suas armas, pintando os quadrados correspondentes
-- É obrigatório que seja deixado ao menos uma posição vazia separando duas armas
 - É utilizado um tabuleiro menor, de dimensão 8x8
 - Cada jogador pode disparar 2 tiros por rodada
 - Os tipos de armas disponíveis são apenas três: submarinos, cruzadores e porta-aviões.
@@ -20,12 +19,14 @@
 ### Jogador
 - Atributos:
   - nome: String
+  - acertos: int
   - meuJogo: BatalhaNaval
-  - jogoDoAdversário: BatalhaNaval
+  - jogoDoAdversário: char[8][8]
 - Métodos:
   - atirar
   - verificarSeAcertou
   - registrarTiro
+  - imprimirTabuleiro
 
 ### BatalhaNaval
 - Atributos:
@@ -33,3 +34,10 @@
 - Métodos:
   - adicionarArmas
   - imprimirTabuleiro
+
+## RodadaDeTiros
+  - Atributos
+    - primeiro jogador
+    - segundo jogador
+  - Métodos
+    - trocaDeTiros
